9098922093f4599baec0fa63178aafb5dc8bc3a1
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-08-20 00:36:59 +0400

    t

---
+++ MetaSharp.Task/MetaSharpTask.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MetaSharp.Tasks {
+    public class MetaSharpTask : ITask {
+        public IBuildEngine BuildEngine { get; set; }
+        public ITaskHost HostObject { get; set; }
+
+        [Required]
+        public ITaskItem[] InputFiles { get; set; }
+        [Output]
+        public ITaskItem[] OutputFiles { get; set; }
+
+        public bool Execute() {
+            for(int i = 0; i < InputFiles.Length; i++)
+                File.WriteAllText(OutputFiles[i].ItemSpec,
+                  ProcessXyzFile(
+                    File.ReadAllText(InputFiles[i].ItemSpec)));
+            return true;
+        }
+
+        private string ProcessXyzFile(string xyzFileContents) {
+            return xyzFileContents;
+        }
+    }
+}
