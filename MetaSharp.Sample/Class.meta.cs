4930039cf60d17a38cc3f828407c326372ae99cd
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-08-20 02:55:03 +0400

    Revert "t"
    This reverts commit 533450a695b858f87d021e7d692b8c678dde8232.

--- MetaSharp.Sample/Class.meta.cs
+++ MetaSharp.Sample/Class.meta.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Gen;
 
 namespace MetaSharp.Sample
 {
     public class Class1
     {
-        public void Do() {
-
-            var t = typeof(Gen.Class_meta_cs);
-        }
     }
 }
--- MetaSharp.Sample/Class.meta.designer.cs
+++
@@ -1,1 +0,0 @@
-namespace Gen { public class Class_meta_cs { public const int Count = 18; } }
