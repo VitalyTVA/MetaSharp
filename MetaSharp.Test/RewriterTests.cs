3362ea464c3686812c4ac05da8938adb937be967
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-09-20 23:36:45 +0400

    t

--- MetaSharp.Test/RewriterTests.cs
+++ MetaSharp.Test/RewriterTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace MetaSharp.Test {
     public class RewriterTests : GeneratorTestsBase {
         [Fact]
         public void RewriteClassName() {
-            var input = @"
+            var input1 = @"
 using MetaSharp;
 namespace MetaSharp.HelloWorld {
     public static class HelloWorldGenerator {
@@ -22,12 +23,33 @@ namespace MetaSharp.HelloWorld {
     }
 }
 ";
-            string output =
+            var input2 = @"
+using MetaSharp;
+namespace MetaSharp.HelloWorld {
+    public static class HelloWorldGenerator2 {
+        public static string MakeBoo() {
+             return ClassGenerator.Class<Boo>();
+        }
+    }
+}
+";
+            string output1 =
 @"    public class Foo {
     }
     public class Moo {
     }";
-            AssertSingleFileSimpleOutput(input, output);
+            string output2 =
+@"    public class Boo {
+    }";
+            var name1 = "file1.meta.cs";
+            var name2 = "file2.meta.cs";
+            AssertMultipleFilesOutput(
+                ImmutableArray.Create(new TestFile(name2, input2), new TestFile(name1, input1)),
+                ImmutableArray.Create(
+                    new TestFile(GetOutputFileName(name1), output1),
+                    new TestFile(GetOutputFileName(name2), output2)
+                )
+            );
         }
     }
 }
