d42cd5abf1861372a05c1343fc8f96cda08f52ab
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-09-30 01:43:37 +0400

    t

---
+++ MetaSharp.Generator/Completer.cs
@@ -0,0 +1,41 @@
+﻿using MetaSharp.Native;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaSharp {
+    public static class Completer {
+        internal static ImmutableArray<Output> GetCompletions(CSharpCompilation compilation, Environment environment) {
+            var prototypes = compilation.GetFiles<MetaProtoAttribute>(environment);
+            var compilationWithPrototypes = compilation.AddSyntaxTrees(prototypes.Keys);
+            //TODO check syntax errors first
+
+            return prototypes
+                .Select(pair => {
+                    var tree = pair.Key;
+                    var model = compilationWithPrototypes.GetSemanticModel(tree);
+                    var classSyntaxes = tree.GetRoot().DescendantNodes(x => !(x is ClassDeclarationSyntax)).OfType<ClassDeclarationSyntax>();
+                    var text = classSyntaxes
+                        .Select(x => model.GetDeclaredSymbol(x))
+                        .Where(x => x.HasAttribute<MetaCompleteClassAttribute>(compilationWithPrototypes))
+                        .Select(type => {
+                            var properties = type.GetMembers().OfType<IPropertySymbol>();
+                            var context = type.Location().CreateContext(type.ContainingNamespace.ToString());
+                            var generator = properties.Aggregate(
+                                new ClassGenerator_(type.Name, ClassModifiers.Partial, skipProperties: true),
+                                (acc, p) => acc.Property(p.Type.Name, p.Name) //TODO use simple type name
+                            );
+                            return context.WrapMembers(generator.Generate());
+                        }).ConcatStringsWithNewLines();
+                    return new Output(text, OutputFileName.Create(pair.Value, environment, MetaLocationKind.IntermediateOutput)); //TODO specify output destination (g.i.cs, etc.)
+                })
+                .ToImmutableArray();
+        }
+    }
+}
--- MetaSharp.Generator/Rewriter.cs
+++ MetaSharp.Generator/Rewriter.cs
@@ -10,34 +10,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MetaSharp {
-    public static class Completer {
-        internal static ImmutableArray<Output> GetCompletions(CSharpCompilation compilation, Environment environment) {
-            var prototypes = compilation.GetFiles<MetaProtoAttribute>(environment);
-            var compilationWithPrototypes = compilation.AddSyntaxTrees(prototypes.Keys);
-            //TODO check syntax errors first
-
-            return prototypes
-                .Select(pair => {
-                    var tree = pair.Key;
-                    var model = compilationWithPrototypes.GetSemanticModel(tree);
-                    var classSyntaxes = tree.GetRoot().DescendantNodes(x => !(x is ClassDeclarationSyntax)).OfType<ClassDeclarationSyntax>();
-                    var text = classSyntaxes
-                        .Select(x => model.GetDeclaredSymbol(x))
-                        .Where(x => x.HasAttribute<MetaCompleteClassAttribute>(compilationWithPrototypes))
-                        .Select(type => {
-                            var properties = type.GetMembers().OfType<IPropertySymbol>();
-                            var context = type.Location().CreateContext(type.ContainingNamespace.ToString());
-                            var generator = properties.Aggregate(
-                                new ClassGenerator_(type.Name, ClassModifiers.Partial, skipProperties: true),
-                                (acc, p) => acc.Property(p.Type.Name, p.Name) //TODO use simple type name
-                            );
-                            return context.WrapMembers(generator.Generate());
-                        }).ConcatStringsWithNewLines();
-                    return new Output(text, OutputFileName.Create(pair.Value, environment, MetaLocationKind.IntermediateOutput)); //TODO specify output destination (g.i.cs, etc.)
-                })
-                .ToImmutableArray();
-        }
-    }
     public static class Rewriter {
         public static ImmutableArray<TreeReplacement> GetReplacements(CSharpCompilation compilation, IEnumerable<SyntaxTree> trees) {
             //TODO remove trees argument (need add included files to trees dictionary in generator and rewrite code in them as well)
