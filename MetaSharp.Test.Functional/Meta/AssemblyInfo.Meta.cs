2be56004aa236c9c18f55f3d7e6b4a362d06714e
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-10-02 01:43:33 +0400

    t

--- MetaSharp.Test.Functional/Meta/AssemblyInfo.Meta.cs
+++ MetaSharp.Test.Functional/Meta/AssemblyInfo.Meta.cs
@@ -1,3 +1,4 @@
 ﻿using MetaSharp;
 
 [assembly: MetaReference(@"..\..\..\Bin\System.Collections.Immutable.dll")]
+//[assembly: MetaProto("POCOViewModels.cs", MetaLocationKind.Designer)]
---
+++ MetaSharp.Test.Functional/Meta/POCOViewModels.cs
@@ -0,0 +1,21 @@
+﻿using MetaSharp;
+namespace MetaSharp.Test.Meta.POCO {
+    using System.Windows;
+    [MetaCompleteViewModel]
+    public class POCOViewModel {
+        internal string NotPublicProperty { get; set; }
+        public string NotVirtualProperty { get; set; }
+        public virtual string ProtectedGetterProperty { protected internal get; set; }
+        public virtual string InternalSetterProperty { get; internal set; }
+        string notAutoImplementedProperty;
+        public virtual string NotAutoImplementedProperty { get { return notAutoImplementedProperty; } set { notAutoImplementedProperty = value; } }
+
+        public virtual string Property1 { get; set; }
+        public virtual string Property2 { get; set; }
+        public virtual object Property3 { get; set; }
+        public virtual int Property4 { get; set; }
+        public virtual Point Property5 { get; set; }
+        public virtual int? Property6 { get; set; }
+        public virtual string ProtectedSetterProperty { get; protected internal set; }
+    }
+}
