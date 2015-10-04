8860186a6cd535ebbe4c5e0aa8a2b2f41cfa6bdb
VitalyTVA <vitalytva@yandex.ru>
2015-10-04 20:10:17 +0300

    t

--- MetaSharp.Test.Functional/Meta/AssemblyInfo.Meta.cs
+++ MetaSharp.Test.Functional/Meta/AssemblyInfo.Meta.cs
@@ -1,5 +1,5 @@
 ﻿using MetaSharp;
 
 [assembly: MetaReference(@"..\..\Bin\System.Collections.Immutable.dll")]
-[assembly: MetaReference(@"..\..\MetaSharp.Test\Bin\Debug\MetaSharp.Test.dll")]
+[assembly: MetaReference(@"..\..\MetaSharp.Test\Bin\MetaSharp.Test.dll")]
 [assembly: MetaProto("POCOViewModels.cs", MetaLocationKind.Designer)]
