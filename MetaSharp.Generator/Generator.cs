07f4748196a2007fc5355a63fb9816a34194f1ba
VitalyTVA <vitalytva@yandex.ru>
2015-10-06 23:25:25 +0300

    t

--- MetaSharp.Generator/Generator.cs
+++ MetaSharp.Generator/Generator.cs
@@ -14,6 +14,7 @@ using System.Reflection;
 using System.Text;
 
 namespace MetaSharp {
+    //TODO show verbose error in correct position when reference or include file or etc. is not found
     //TODO Conditional("METASHARP") attribute for all meta attributes so they do not go to final assembly
     //TODO push active configuration/compile constants (DEBUG, etc.) to meta code to make same evnironment in meta code as in main
     //TODO exceptions in generator methods (+show line where exception occured)
