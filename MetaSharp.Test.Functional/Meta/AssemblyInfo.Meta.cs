4e84a095f663a2b75a8b1680a38f92748637ab87
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-10-20 01:22:13 +0400

    t

--- MetaSharp.Test.Functional/Meta/AssemblyInfo.Meta.cs
+++ MetaSharp.Test.Functional/Meta/AssemblyInfo.Meta.cs
@@ -4,12 +4,11 @@ using System.Collections.Generic;
 [assembly: MetaReference(@"..\..\Bin\System.Collections.Immutable.dll")]
 [assembly: MetaReference(@"..\..\MetaSharp.Test\Bin\MetaSharp.Test.dll")]
 
-namespace MetaSharp.Test.Meta {
-    public static class CompleteFiles {
-        [MetaLocation(MetaLocationKind.Designer)]
-        public static Either<IEnumerable<MetaError>, Output> CompletePOCOModels(MetaContext context) {
-            return context.Complete("POCOViewModels.cs")
-                .Select(text => new Output(text, "POCOViewModels.designer.cs"));
-        }
+ public static class CompleteFiles {
+    [MetaLocation(MetaLocationKind.Designer)]
+    public static Either<IEnumerable<MetaError>, Output> CompletePOCOModels(MetaContext context) {
+        return context.Complete("POCOViewModels.cs")
+        .Select(text => new Output(text, "POCOViewModels.designer.cs"));
     }
 }
+
