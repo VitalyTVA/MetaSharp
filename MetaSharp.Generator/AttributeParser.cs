4e2951ac16e4b8aac1c5ac4e4ca01c0318f64deb
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-10-26 20:10:18 +0400

    t

---
+++ MetaSharp.Generator/AttributeParser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaSharp {
+    public static class AttributeParser {
+        public static T Parse<T>(IPropertySymbol property) where T : class {
+            var attrubutes = ((PropertyDeclarationSyntax)property.Node())
+                .AttributeLists
+                .SelectMany(x => x.Attributes)
+                .Where(x => x.Name.ToString() == typeof(T).Name || x.Name.ToString() == typeof(T).Name + "Attribute");
+            if(!attrubutes.Any())
+                return null;
+            return Activator.CreateInstance(typeof(T)) as T;
+        }
+    }
+}
---
+++ MetaSharp.Test/AttributeParserTests.cs
@@ -0,0 +1,49 @@
+﻿using MetaSharp.Native;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MetaSharp.Test {
+    public class AttrubteParserTests {
+        public class BindableProperty {
+            public readonly bool IsBindable;
+            public BindableProperty() {
+            }
+            public BindableProperty(bool isBindable) {
+                IsBindable = isBindable;
+            }
+            public override bool Equals(object obj) {
+                var other = obj as BindableProperty;
+                return other != null && other.IsBindable == IsBindable;
+            }
+            public override int GetHashCode() {
+                throw new NotSupportedException();
+            }
+        }
+        [Fact]
+        public void ParseSimple() {
+            AssertParseAttributes("[Bindable(true)]", new BindableProperty());
+            AssertParseAttributes("[BindableAttribute]", new BindableProperty());
+            AssertParseAttributes("[Bindable_]", default(BindableProperty));
+        }
+
+        void AssertParseAttributes<T>(string attributes, T expected) where T : class {
+            var code = $@"
+public class TestClass {{
+    {attributes}
+    public string TestProperty {{ get; set; }}
+}}
+";
+            var compilation = CSharpCompilation.Create("test", SyntaxFactory.ParseSyntaxTree(code).Yield());
+            var property = compilation.GetTypeByMetadataName("TestClass").Properties().Single();
+            var parsed = AttributeParser.Parse<T>(property);
+            Assert.Equal(expected, parsed);
+        }
+    }
+}
